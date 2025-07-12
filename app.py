c39e5e44e2266b6bb34b9e84ea8e42d9ec8fc1d1
Rohitwagh1406 <rohitwagh1406@gmail.com>
2025-07-12 16:10:16 +0530

    Data Ingestion

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomeException
+from src.mlproject.components.data_ingesion import DataIngstion
+from src.mlproject.components.data_ingesion import DataIngestionConfig
 import sys
 
 
@@ -7,7 +9,9 @@ if __name__ =="__main__":
     logging.info("The execution has started !")
 
     try:
-        a=1/0
+        # data_ingesion_config=DataIngestionConfig()
+        data_ingesion=DataIngstion()
+        data_ingesion.initiate_data_ingestion()
     except Exception as e:
         logging.info("Custome Exception")
         raise CustomeException(e,sys)
